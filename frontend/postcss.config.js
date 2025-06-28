fb799c953e829a43c050ce81c695edfa6c9e912f
Yash Lahane <heyyaash01@gmail.com>
2025-06-28 15:51:05 +0530

    Inital Commit

---
+++ frontend/postcss.config.js
@@ -0,0 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+import autoprefixer from "autoprefixer";
+import { plugin } from "postcss";
+
+export default {
+    plugins:{
+        tailwindcss:{},
+        autoprefixer:{},
+    },
+}
